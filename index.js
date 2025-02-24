b9ae29d4e34de1c0c57be3ce7c47b69a7e15fe11
Hirwa Rene <hirwarene6@gmail.com>
2025-02-24 20:47:51 +0200

    added class click-event

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 //selector
 
+//document.querySelector('h4').style.background="red";
+const balise = document.querySelector('h4');
+
+console.log(balise)
+
+const btn = document.querySelector('#btn1');
+
+console.log(btn)
