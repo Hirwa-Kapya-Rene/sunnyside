efbffd179b0484a8b816d03b1593b9cec828ced6
Hirwa Rene <hirwarene6@gmail.com>
2025-02-24 21:00:11 +0200

    added br of 100px to click-event

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 //selector
 
 //document.querySelector('h4').style.background="red";
-const balise = document.querySelector('h4');
+/*const balise = document.querySelector('h4');
 
 console.log(balise)
 
 const btn = document.querySelector('#btn1');
 
-console.log(btn)
+console.log(btn)*/
+
+const questContainer = document.querySelector('.click-event');
+
+questContainer.style.borderRadius = "100px";
